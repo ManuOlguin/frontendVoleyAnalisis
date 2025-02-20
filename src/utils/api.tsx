eca5293c1018204a64a12ebcc207600d4c2c1e53
MANUEL OLGUIN <olguin.manuel@maimonidesvirtual.com.ar>
2025-02-20 14:09:31 -0300

    init

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:5000';
+const API_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URLE;
 
 // Example: Fetch all players
 export const fetchPlayers = async () => {
